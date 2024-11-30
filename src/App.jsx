620d3f2f4a66665ce2754a50272ff088573163a6
Ishan Shrestha <iahan.esan18@gmail.com>
2024-11-30 13:33:47 +0545

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Home from "./pages/Home"
+import Signin from "./pages/Signin"
 
-const App = () => {
+export default  function App () {
   return (
-    <h1 className='text-red-500'>
-      App
-      
-    </h1>
+   <BrowserRouter>
+   <Routes>
+   <Route path ="/" element = {<Home />}/>
+   <Route path ="/sign-in" element = {<Signin />}/>
+   <Route path ="/sign-up" element = {<SignUp />}/>
+   <Route path ="/about" element = {<About />}/>
+   <Route path ="/profile" element = {<Profile />}/>
+
+   </Routes>
+   </BrowserRouter>
   )
 }
 
-export default App
+
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Signin = () => {
+  return (
+    <div>
+      SIgnUin
+    </div>
+  )
+}
+
+export default Signin
+SIgnUin
